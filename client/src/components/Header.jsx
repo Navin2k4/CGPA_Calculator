56375ade68f71cd2cfb4cf2bd12c774101cd2124
Navin Kumaran <navinkumaran2004@gmail.com>
2024-05-07 20:26:02 +0530

    Design changes

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,24 +2,19 @@ import React from 'react';
 import { Navbar, Dropdown, DropdownItem } from 'flowbite-react';
 
 const Header = () => {
-    const gradeScale = { 'O': 10, 'A+': 9, 'A': 8, 'B+': 7, 'B': 6, 'C': 5, 'U': 0 };
+    const gradeScale = { 'O': '10 Points', 'A+': '9 Points', 'A': '8 Points', 'B+': '7 Points', 'B': '6 Points', 'C': '5 Points', 'U': 'Absent / Arrear' };
 
     return (
         <Navbar className=" bg-slate-200 ring-2 ring-slate-300 ">
-            < img className='w-16 h-16 rounded-full' src='vcet.jpeg' />
+            <img className='w-16 h-16 rounded-full' src='vcet.jpeg' />
             <h1 className='text-2xl font-bold font-mono'>CGPA CALCULATOR</h1>
             <Dropdown inline label="Grades">
                 {Object.keys(gradeScale).map((grade) => (
                     <DropdownItem key={grade}>
-                        <div>
-                            <table>
-                                <tr>
-                                    <td>{grade}</td>
-                                    <td>:</td>
-                                    <td>{gradeScale[grade]}</td>
-                                </tr>
-                            </table>
-
+                        <div className='flex justify-center items-center'>
+                            <p className='mr-2'>{grade}</p>
+                            <p className=''>:</p>
+                            <p className='ml-2'>{gradeScale[grade]}</p>
                         </div>
                     </DropdownItem>
                 ))}
