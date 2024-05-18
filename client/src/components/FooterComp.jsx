5f41a942e43c14a239613d6d4003e7e88dc7921d
Navin Kumaran <navinkumaran2004@gmail.com>
2024-05-18 19:05:43 +0530

    Footer Updated

--- client/src/components/FooterComp.jsx
+++ client/src/components/FooterComp.jsx
@@ -2,9 +2,10 @@ const FooterComp = () => {
     return (
         <footer className="bg-gray-900 text-white py-6">
             <div className="container mx-auto text-center">
-                <p className="text-sm mb-2">&copy; 2024 Navin Kumaran. All Rights Reserved.</p>
-                <p className="text-sm mx-2">Department of Computer Science and Engineering</p>
-                <p className="text-sm m-2">Velemmal College of Engineering and Technology, Madurai</p>
+                <p className="text-[14px] m-2">Velemmal College of Engineering and Technology, Madurai</p>
+                <p className="text-[14px] m-2">Department of Computer Science and Engineering</p>
+                <hr></hr>
+                <p className="text-[12px] m-4">&copy; {new Date().getFullYear()} Navinkumaran O H, 2022-2026 batch, and Mr. S. Murali, AP/CSE. All Rights Reserved.</p>
             </div>
         </footer>
     )
